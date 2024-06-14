a557aa9a59629252e3b6ae957cf21886c8d446ff
mikhawa <michaeljpitz@gmail.com>
2024-06-14 11:24:43 +0200

    structure

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # BlOOG1
+
 Blog en PHP 8 - MVC OO - Travail du Groupe 1
+
+## Installation
+
+1. Créez un fork du projet
+2. Clonez le fork du projet sur votre machine
+3. Créez un upstream pour pouvoir récupérer les mises à jour du projet
+
+
+
+
 
 Avec utilisation du moteur de Template Twig
--- datas/bioog-structure.sql
+++ datas/bioog-structure.sql
@@ -2,7 +2,7 @@
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
 -- Schema bioog
@@ -41,12 +41,12 @@ CREATE TABLE IF NOT EXISTS `bioog`.`user` (
   `user_mail` VARCHAR(180) NOT NULL,
   `user_status` TINYINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '0 pas actif\n1 actif\n2 banni',
   `user_secret_key` VARCHAR(80) NOT NULL,
-  `permission_persmission_id` INT UNSIGNED NOT NULL,
+  `permission_persmission_id` INT UNSIGNED NULL,
   PRIMARY KEY (`user_id`),
   CONSTRAINT `fk_user_permission`
     FOREIGN KEY (`permission_persmission_id`)
     REFERENCES `bioog`.`permission` (`persmission_id`)
-    ON DELETE NO ACTION
+    ON DELETE SET NULL
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
@@ -71,12 +71,12 @@ CREATE TABLE IF NOT EXISTS `bioog`.`article` (
   `article_date_update` TIMESTAMP NULL,
   `article_date_publish` TIMESTAMP NULL,
   `article_is_published` TINYINT UNSIGNED NOT NULL DEFAULT 0,
-  `user_user_id` INT UNSIGNED NOT NULL,
+  `user_user_id` INT UNSIGNED NULL,
   PRIMARY KEY (`article_id`),
   CONSTRAINT `fk_article_user1`
     FOREIGN KEY (`user_user_id`)
     REFERENCES `bioog`.`user` (`user_id`)
-    ON DELETE NO ACTION
+    ON DELETE SET NULL
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
@@ -114,12 +114,12 @@ CREATE TABLE IF NOT EXISTS `bioog`.`article_has_category` (
   CONSTRAINT `fk_article_has_category_article1`
     FOREIGN KEY (`article_article_id`)
     REFERENCES `bioog`.`article` (`article_id`)
-    ON DELETE NO ACTION
+    ON DELETE CASCADE
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_article_has_category_category1`
     FOREIGN KEY (`category_category_id`)
     REFERENCES `bioog`.`category` (`category_id`)
-    ON DELETE NO ACTION
+    ON DELETE CASCADE
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
@@ -151,24 +151,27 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `bioog`.`comment_has_article` ;
 
 CREATE TABLE IF NOT EXISTS `bioog`.`comment_has_article` (
-  `comment_comment_id` INT UNSIGNED NOT NULL,
-  `article_article_id` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`comment_comment_id`, `article_article_id`),
+  `comment_comment_id` INT UNSIGNED NULL,
+  `article_article_id` INT UNSIGNED NULL,
   CONSTRAINT `fk_comment_has_article_comment1`
     FOREIGN KEY (`comment_comment_id`)
     REFERENCES `bioog`.`comment` (`comment_id`)
-    ON DELETE NO ACTION
+    ON DELETE SET NULL
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_comment_has_article_article1`
     FOREIGN KEY (`article_article_id`)
     REFERENCES `bioog`.`article` (`article_id`)
-    ON DELETE NO ACTION
+    ON DELETE SET NULL
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 CREATE INDEX `fk_comment_has_article_article1_idx` ON `bioog`.`comment_has_article` (`article_article_id` ASC) VISIBLE;
 
 CREATE INDEX `fk_comment_has_article_comment1_idx` ON `bioog`.`comment_has_article` (`comment_comment_id` ASC) VISIBLE;
+
+CREATE UNIQUE INDEX `article_article_id_UNIQUE` ON `bioog`.`comment_has_article` (`article_article_id` ASC) VISIBLE;
+
+CREATE UNIQUE INDEX `comment_comment_id_UNIQUE` ON `bioog`.`comment_has_article` (`comment_comment_id` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
@@ -177,24 +180,27 @@ CREATE INDEX `fk_comment_has_article_comment1_idx` ON `bioog`.`comment_has_artic
 DROP TABLE IF EXISTS `bioog`.`comment_has_user` ;
 
 CREATE TABLE IF NOT EXISTS `bioog`.`comment_has_user` (
-  `comment_comment_id` INT UNSIGNED NOT NULL,
-  `user_user_id` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`comment_comment_id`, `user_user_id`),
+  `comment_comment_id` INT UNSIGNED NULL,
+  `user_user_id` INT UNSIGNED NULL,
   CONSTRAINT `fk_comment_has_user_comment1`
     FOREIGN KEY (`comment_comment_id`)
     REFERENCES `bioog`.`comment` (`comment_id`)
-    ON DELETE NO ACTION
+    ON DELETE SET NULL
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_comment_has_user_user1`
     FOREIGN KEY (`user_user_id`)
     REFERENCES `bioog`.`user` (`user_id`)
-    ON DELETE NO ACTION
+    ON DELETE SET NULL
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 CREATE INDEX `fk_comment_has_user_user1_idx` ON `bioog`.`comment_has_user` (`user_user_id` ASC) VISIBLE;
 
 CREATE INDEX `fk_comment_has_user_comment1_idx` ON `bioog`.`comment_has_user` (`comment_comment_id` ASC) VISIBLE;
+
+CREATE UNIQUE INDEX `comment_comment_id_UNIQUE` ON `bioog`.`comment_has_user` (`comment_comment_id` ASC) VISIBLE;
+
+CREATE UNIQUE INDEX `user_user_id_UNIQUE` ON `bioog`.`comment_has_user` (`user_user_id` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
@@ -206,12 +212,12 @@ CREATE TABLE IF NOT EXISTS `bioog`.`image` (
   `image_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `image_url` VARCHAR(60) NOT NULL,
   `image_description` VARCHAR(150) NULL,
-  `article_article_id` INT UNSIGNED NOT NULL,
+  `article_article_id` INT UNSIGNED NULL,
   PRIMARY KEY (`image_id`),
   CONSTRAINT `fk_image_article1`
     FOREIGN KEY (`article_article_id`)
     REFERENCES `bioog`.`article` (`article_id`)
-    ON DELETE NO ACTION
+    ON DELETE SET NULL
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
@@ -238,24 +244,27 @@ CREATE UNIQUE INDEX `tag_slug_UNIQUE` ON `bioog`.`tag` (`tag_slug` ASC) VISIBLE;
 DROP TABLE IF EXISTS `bioog`.`tag_has_article` ;
 
 CREATE TABLE IF NOT EXISTS `bioog`.`tag_has_article` (
-  `tag_tag_id` INT UNSIGNED NOT NULL,
-  `article_article_id` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`tag_tag_id`, `article_article_id`),
+  `tag_tag_id` INT UNSIGNED NULL,
+  `article_article_id` INT UNSIGNED NULL,
   CONSTRAINT `fk_tag_has_article_tag1`
     FOREIGN KEY (`tag_tag_id`)
     REFERENCES `bioog`.`tag` (`tag_id`)
-    ON DELETE NO ACTION
+    ON DELETE CASCADE
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_tag_has_article_article1`
     FOREIGN KEY (`article_article_id`)
     REFERENCES `bioog`.`article` (`article_id`)
-    ON DELETE NO ACTION
+    ON DELETE CASCADE
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 CREATE INDEX `fk_tag_has_article_article1_idx` ON `bioog`.`tag_has_article` (`article_article_id` ASC) VISIBLE;
 
 CREATE INDEX `fk_tag_has_article_tag1_idx` ON `bioog`.`tag_has_article` (`tag_tag_id` ASC) VISIBLE;
+
+CREATE UNIQUE INDEX `article_article_id_UNIQUE` ON `bioog`.`tag_has_article` (`article_article_id` ASC) VISIBLE;
+
+CREATE UNIQUE INDEX `tag_tag_id_UNIQUE` ON `bioog`.`tag_has_article` (`tag_tag_id` ASC) VISIBLE;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;

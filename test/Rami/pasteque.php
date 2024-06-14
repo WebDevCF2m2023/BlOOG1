f9460d4d14b2ed39af0a4dfd8870f39c332869e3
rammroumm2K <ramybrz00@gmail.com>
2024-06-14 16:06:29 +0200

    02

--- test/Rami/pasteque.php
+++ test/Rami/pasteque.php
@@ -10,12 +10,47 @@ spl_autoload_register(function ($class) {
 
 class TestAbstractMapping extends AbstractMapping
 {
+    protected ?string $article_title;
+    protected ?string $article_date_update;
+
     public function __construct(array $tab)
     {
         parent::__construct($tab);
     }
 
-   
+    public function setArticleTitle(string $value): void
+    {
+        $this->article_title = $value;
+    }
+
+    public function setArticleDateUpdate(string $value): void
+    {
+        $this->article_date_update = $value;
+    }
+
+    protected function hydrate(array $assoc): void
+    {
+        // tant qu'on a des éléments dans le tableau
+        foreach ($assoc as $clef => $valeur) {
+            // on explose la clef où on trouve des _
+            $tab = explode("_",$clef);
+            // on mets en majuscule le nom de chaque clef du tableau 
+            // array_map est un raccouci pour appliquer une fonction 
+            // à chaque élément d'un tableau (+ rapide que foreach)
+            $majuscule = array_map('ucfirst',$tab);
+            // on remet le tout ensemble (on implose quoi)
+            $nouveauNomEnCamelCase = implode($majuscule);
+            // création du nom de la méthode de type setter
+            $methodeName = "set" . $nouveauNomEnCamelCase;
+            
+            // si la méthode existe
+            if (method_exists($this, $methodeName)) {
+                // on hydrate le paramètre avec la valeur
+                $this->$methodeName($valeur);
+            }
+        }
+    }
+
 }
 
 $test = new TestAbstractMapping([
@@ -24,5 +59,8 @@ $test = new TestAbstractMapping([
     'article_title'=>"un titre",
     'article_date_update'=>"2024-03-17 21:45",
 ]);
+
+var_dump($test);
+
 
 

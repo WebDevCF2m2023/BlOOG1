c63eca7e47a4b150f8fd3217d898dcc08ffb14d8
Anthoonyyy <Anthony212007@hotmail.com>
2024-06-14 16:04:51 +0200

    a corriger

--- test/Anthony/01-base.php
+++ test/Anthony/01-base.php
@@ -10,16 +10,32 @@ spl_autoload_register(function ($class) {
 
 class TestAbstractMapping extends AbstractMapping
 {
+    protected ?string $article_title;
+    protected ?string $article_date_update;
+
     public function __construct(array $tab)
     {
         parent::__construct($tab);
     }
+
+    public function setArticleTitle(string $value):void{
+        $this->article_title = $value;
+    }
+   
+
     protected function hydrate(array $assoc): void
     {
         // tant qu'on a des éléments dans le tableau
         foreach ($assoc as $clef => $valeur) {
-            // création du nom de la méthode de type setter
-            $methodeName = "set" . str_replace("_", "", ucfirst($clef));
+
+            explode("_", $clef);
+
+           $majuscule =  array_map("ucfirst",$tab);
+
+           $nouveauNomEnCamelCase =  implode($majuscule);
+              // création du nom de la méthode de type setter
+
+            $methodeName = "set" . $nouveauNomEnCamelCase;
             // si la méthode existe
             if (method_exists($this, $methodeName)) {
                 $this->$methodeName($valeur);
@@ -35,5 +51,7 @@ class TestAbstractMapping extends AbstractMapping
 $test = new TestAbstractMapping(['bla_bla_bla' => 'test',
                                 'test_coucou ' => 'youpi',
                                 'article_title' =>'un titre']);
+
+  var_dump($test);
 
 

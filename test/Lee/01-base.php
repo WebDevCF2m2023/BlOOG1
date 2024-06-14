7797ee8f86e2bca61f5e02d3aa8f1c251d6c265c
leerlandais <leerlandais@gmail.com>
2024-06-14 15:35:28 +0200

    surcharge

--- test/Lee/01-base.php
+++ test/Lee/01-base.php
@@ -14,14 +14,24 @@ class TestAbstractMapping extends AbstractMapping
     {
         parent::__construct($tab);
     }
-
+    protected function hydrate(array $assoc): void
+    {
+        // tant qu'on a des éléments dans le tableau
+        foreach ($assoc as $clef => $valeur) {
+            // création du nom de la méthode de type setter
+            $methodeName = "set" . str_replace("_", "", ucfirst($clef));
+            // si la méthode existe
+            if (method_exists($this, $methodeName)) {
+                $this->$methodeName($valeur);
+            }else{
+                // sinon on affiche un message d'erreur
+                echo "La méthode $methodeName n'existe pas<br>";
+            }
+        }
+    }
 
 }
 
-$test = new TestAbstractMapping([
-    'test_poi_lulu' => 'test',
-    'test_coucou' => "youpie",
-    'art_title' => 'dsfs    '
-
-]);
-
+$test = new TestAbstractMapping(['bla_bla_bla' => 'test',
+                                'test_coucou ' => 'youpi',
+                                'article_title' =>'un titre']);

204e265485b446ad06af295a2b96f5ad0aee4e41
Guillaume <guillaumebaillieux88@gmail.com>
2024-06-14 14:24:35 +0200

    Maj01Base

--- test/Michael/01-base.php
+++ test/Michael/01-base.php
@@ -19,6 +19,23 @@ class TestAbstractMapping extends AbstractMapping
     {
         echo 'Test';
     }
+
+    // création de notre hydratation, en partant d'un tableau associatif et de ses clefs, on va régénérer le nom des setters existants dans les classes enfants
+    protected function hydrate(array $assoc): void
+    {
+        // tant qu'on a des éléments dans le tableau
+        foreach ($assoc as $clef => $valeur) {
+            // création du nom de la méthode de type setter
+            $methodeName = "set" . str_replace("_", "", ucfirst($clef));
+            // si la méthode existe
+            if (method_exists($this, $methodeName)) {
+                $this->$methodeName($valeur);
+            }else{
+                // sinon on affiche un message d'erreur
+                echo "La méthode $methodeName n'existe pas";
+            }
+        }
+    }
 }
 
 $test = new TestAbstractMapping(['test_poi_lulu' => 'test']);

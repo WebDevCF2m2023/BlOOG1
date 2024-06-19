4a5edd2913ccce6881c4f8c1aeb00c60351b8b4e
Tevin01 <tevin.eliya.bofona@cf2000.onmicrosoft.com>
2024-06-19 15:46:11 +0200

    Image&Permission mapping

---
+++ test/Tevin/02-mapping/model/Mapping/PermissionMapping.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace model\Mapping;
+
+use model\Abstract\AbstractMapping;
+use DateTime;
+use Exception;
+
+class PermissionMapping extends AbstractMapping
+{
+    // Les propriétés de la classe sont le nom des
+    // attributs de la table Exemple (qui serait en
+    // base de données)
+    protected ?int $permission_id;
+    protected ?string $permission_name;
+    protected ?string $permission_description;
+
+    // Les getters et setters
+    // Les getters permettent de récupérer la valeur
+    // d'un attribut de la classe
+
+    // Les setters permettent de modifier la valeur
+    // d'un attribut de la classe, en utilisant l'hydratation
+    // venant de la classe AbstractMapping
+    public function getPermissionId(): ?int
+    {
+        return $this->permission_id;
+    }
+
+    public function setPermissionId(?int $permission_id): void
+    {
+        $this->permission_id = $permission_id;
+    }
+
+    public function getPermissionName(): ?string
+    {
+        return $this->permission_name;
+    }
+
+    public function setPermissionName(?string $permission_name): void
+    {
+        $this->permission_name = $permission_name;
+    }
+
+    public function getPermissionDescription(): ?string
+    {
+        $permission = htmlspecialchars(strip_tags(trim($_POST['permission_description'])),ENT_QUOTES);
+        // Gimme two points if that's actually all it takes
+
+        return $this->permission_description;
+    }
+
+    public function setPermissionDescription(?string $permission_description): void
+    {
+        $this->permission_description = $permission_description;
+    }
+
+}

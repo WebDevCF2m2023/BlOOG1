c2ba3d897e2a7d8f3f8d483f0242a33b6920e69d
Michael J. Pitz <michaeljpitz@gmail.com>
2024-06-20 09:47:47 +0200

    Update ExempleMapping.php
    il faut retirer les méthodes inutiles

--- test/Marwan/02-mapping/model/Mapping/ExempleMapping.php
+++ test/Marwan/02-mapping/model/Mapping/ExempleMapping.php
@@ -54,47 +54,4 @@ class PermissionMapping extends AbstractMapping
     }
 
 
-    public function setExempleDate(null|string|DateTime $exemple_date): void
-    {
-        // si c'est une chaine de caractère
-        if(is_string($exemple_date)){
-            try {
-                // on essaye de convertir la date en objet DateTime
-                $exemple_date = new DateTime($exemple_date);
-                $this->exemple_date = $exemple_date->format("Y-m-d H:i:s");
-            } catch (Exception $e) {
-                // en cas d'échec, on met la date à null
-                $this->exemple_date = null;
-            }
-        // si c'est un objet (DateTime seul possible)
-        }elseif (is_object($exemple_date)){
-            // on formate la date en string en DATETIME
-            $this->exemple_date = $exemple_date->format("Y-m-d H:i:s");
-        }else{
-            $this->exemple_date = null;
-        }
-
-    }
-
-    public function getExempleBoolean(): ?bool
-    {
-        return $this->exemple_boolean;
-    }
-
-    public function setExempleBoolean(?bool $exemple_boolean): void
-    {
-        $this->exemple_boolean = $exemple_boolean;
-    }
-
-    public function getExempleFloat(): ?float
-    {
-        return $this->exemple_float;
-    }
-
-    public function setExempleFloat(?float $exemple_float): void
-    {
-        $this->exemple_float = $exemple_float;
-    }
-
-
 }

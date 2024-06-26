986a254989e6066461b5f406374e3432364e4d16
BailGui <guillaumebaillieux88@gmail.com>
2024-06-26 12:37:39 +0200

    insertViewImage

---
+++ test/Guillaume/04-Manager/view/image/insertImage.view.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemple du ImageManager::insert()</title>
+</head>
+<body>
+    <h1>Exemple du ImageManager::insert()</h1>
+    <div>
+        <?php
+        require 'menu.image.view.php';
+        if(isset($error)) echo "<h4>$error</h4>";
+        ?>
+    <h3>Insertion d'une image</h3>
+    <form action="" method="post">
+        <label for="image_url">Image</label>
+        <textarea name="image_url" id="image_url" cols="30" rows="10"></textarea>
+        <input type="submit" value="Envoyer">
+    </form>
+
+    </div>
+
+</body>
+</html>

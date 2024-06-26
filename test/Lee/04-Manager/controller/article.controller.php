e7c167a3b0ee17b3143d9df6ccc6635d28679e15
leerlandais <leerlandais@gmail.com>
2024-06-26 07:05:24 +0100

    corrected lang=fr

--- test/Lee/04-Manager/controller/article.controller.php
+++ test/Lee/04-Manager/controller/article.controller.php
@@ -7,19 +7,26 @@ $articleManager = new ArticleManager($dbConnect);
 
 
 if(isset($_GET['action']) 
-    && ($_GET['action'] == "delete")
-        && ctype_digit($_GET["id"])){
-    $idArticle = (int) $_GET['id'];
-    // delete comment
-    $deleteArticle = $articleManager->delete($idArticle);
-    if($deleteArticle===true) {
-        header("Location: ?article");
-        exit();
-    }else{
-        $error = $deleteArticle;
-    }
-
-}
+&& ($_GET['action'] == "delete")
+        && ctype_digit($_GET["id"])
+        ){
+            $idArticle = (int) $_GET['id'];
+            // delete comment
+            $deleteArticle = $articleManager->delete($idArticle);
+            if($deleteArticle===true) {
+                header("Location: ?article");
+                exit();
+            }else{
+                $error = $deleteArticle;
+            }
+            
+        }else if (isset($_GET["action"])
+        && ($_GET["action"] == "update")
+        && ctype_digit($_GET["id"])
+        ){
+            $artId = $_GET["id"];
+            $selectOneArticle = $articleManager->selectOneById($artId);
+        }
 
 $selectAllArticles = $articleManager->selectAll();
 require ('../view/article/SelectAllArticle.php');

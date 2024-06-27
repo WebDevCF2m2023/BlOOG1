9b47ba2011648f52fea97d4942e52144af17916a
Helioz7 <baris.aktas@cf2000.onmicrosoft.com>
2024-06-27 15:53:32 +0200

    update

--- test/Baris/04-Manager/view/Category/selectOneComment.view.php
+++ test/Baris/04-Manager/view/Category/selectOneComment.view.php
@@ -6,39 +6,29 @@
     <title>Exemple du CategoryManager::selectOneCategoryt()</title>
 </head>
 <body>
-<div class="container">
-        <h1>Exemple de CategoryManager::selectOneCategory()</h1>
-        <div>
-            <?php require 'menu.comment.view.php'; ?>
+    <h1>Exemple du CategoryManager::selectOneCategoryt()</h1>
+    <div>
+        <?php
 
-            <?php if(is_null($selectOneCategory)): ?>
-                <h3 class="error-message">Commentaire inexistant</h3>
-            <?php else: ?>
-                <div class="comment-details">
-                    <h4>ID : <?= $selectOneCategory->getCategoryId() ?> 
-                        <a href="?view=<?= $selectOneCategory->getCategoryId() ?>">Voir ce commentaire via son id</a>
-                    </h4>
-                    <p><strong>Slug :</strong> <?= $selectOneCategory->getCategorySlug() ?></p>
-                    <p><strong>Description :</strong> <?= $selectOneCategory->getCategoryDescription() ?></p>
-                </div>
-                <hr>
-                <form action="" method="post">
-                    <label for="category_id">ID</label>
-                    <input type="text" name="category_id" id="category_id" value="<?= htmlspecialchars($selectOneCategory->getCategoryId()) ?>" readonly>
+        require 'menu.comment.view.php';
 
-                    <label for="category_slug">Lien</label>
-                    <input type="text" name="category_slug" id="category_slug" value="<?= htmlspecialchars($selectOneCategory->getCategorySlug()) ?>">
-
-                    <label for="category_name">Nom</label>
-                    <input type="text" name="category_name" id="category_name" value="<?= htmlspecialchars($selectOneCategory->getCategoryName()) ?>">
-
-                    <label for="category_description">Description</label>
-                    <textarea name="category_description" id="category_description" cols="30" rows="10"><?= htmlspecialchars($selectOneCategory->getCategoryDescription()) ?></textarea>
-
-                    <input type="submit" value="Mettre à jour">
-                </form>
-            <?php endif; ?>
-        </div>
+        if(is_null($selectOneCategory)):
+        ?>
+        <h3>Commentaire inexistant</h3>
+        
+        <?php
+    else:
+        ?>
+    <h4>ID : <?=$selectOneCategory->getCategoryId()?> <a href="?view=<?=$selectOneCategory->getCategoryId()?>">Voir cette catégorie via son id</a></h4>
+    <p><?=$selectOneCategory->getCategorySlug ()?></p>
+    <p><?=$selectOneCategory->getCategoryDescription()?></p><hr>
+        <?php
+    endif;
+        ?>
     </div>
+    
+    <?php
+var_dump($dbConnect,$CategoryManager,$selectOneCategory);
+    ?>
 </body>
 </html>

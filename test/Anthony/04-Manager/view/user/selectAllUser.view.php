c70f2b49f503fce1ce91745b9ca456f79c8b6049
Anthoonyyy <Anthony212007@hotmail.com>
2024-07-01 15:10:02 +0200

    appel du controleur pour tester fonction

--- test/Anthony/04-Manager/view/user/selectAllUser.view.php
+++ test/Anthony/04-Manager/view/user/selectAllUser.view.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<?php require "../controller/publicController.php"; ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -80,6 +80,7 @@
                         </tr>
                     <?php
                     endforeach;
+                    
                     ?>
                 </tbody>
             </table>

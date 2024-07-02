46fff46eb216f01fb37e640106b50af9e692cd0f
mikhawa <michaeljpitz@gmail.com>
2024-07-02 16:20:03 +0200

    is Environment Twig

--- public/index.php
+++ public/index.php
@@ -3,6 +3,10 @@
 // session
 session_start();
 
+// chemin vers Twig
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
 // Appel de la config
 require_once "../config.php";
 
@@ -11,5 +15,18 @@ spl_autoload_register(function ($class) {
     $class = str_replace('\\', '/', $class);
     require PROJECT_DIRECTORY.'/' .$class . '.php';
 });
+
+// chargement de l'autoload de composer
+require_once PROJECT_DIRECTORY.'/vendor/autoload.php';
+
+// chemin vers les templates twig
+$loader = new FilesystemLoader(PROJECT_DIRECTORY.'/view/');
+// création d'une instance de $twig
+$twig = new Environment($loader, [
+    'cache' => false, // pas de cache en dev
+    // 'cache' => '/path/to/compilation_cache', // chemin du cache pour la prod
+    // activation du debug en dev
+    'debug' => true,
+]);
 
 echo "<h1>En construction</h1>";

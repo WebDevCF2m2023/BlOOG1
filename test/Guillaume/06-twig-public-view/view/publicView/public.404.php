09f1c9ced011bbd733e8c32f97070af3505fcf7e
BailGui <guillaumebaillieux88@gmail.com>
2024-07-05 17:09:42 +0200

    maj404

--- test/Guillaume/06-twig-public-view/view/publicView/public.404.php
+++ test/Guillaume/06-twig-public-view/view/publicView/public.404.php
@@ -1,19 +1,30 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Bloog 1 | Erreur 404</title>
-</head>
-<body>
-    <h1>Bloog 1 | Erreur 404</h1>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Blog Post - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+    <h1 class="fw-bolder mb-1">Bloog 1 | Erreur 404</h1>
     <?php
     require 'public.menu.php';
     ?>
-    <h2>Erreur 404</h2>
+    <h2 class="fw-bolder mb-4 mt-5">Erreur 404</h2>
     <p>La page demandée n'existe pas</p>
-</body>
+ <!-- Footer-->
+ <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
 </html>

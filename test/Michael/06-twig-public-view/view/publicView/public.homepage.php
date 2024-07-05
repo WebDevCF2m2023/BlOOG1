88c872fb3c5c847201ff3d1c93ef5b13c4ea9320
mikhawa <michaeljpitz@gmail.com>
2024-07-05 10:58:46 +0200

    homepage ok

--- test/Michael/06-twig-public-view/view/publicView/public.homepage.php
+++ test/Michael/06-twig-public-view/view/publicView/public.homepage.php
@@ -12,6 +12,44 @@
     <h1>Bloog 1 | Homepage</h1>
     <?php
     require 'public.menu.php';
+    ?>
+    <h2>Les derniers articles</h2>
+    <?php
+    if($articles === null):
+    ?>
+    <h3>Aucun article n'a été trouvé</h3>
+    <?php
+    else:
+        foreach ($articles as $article):
+            ?>
+            <article>
+                <h3><?= $article->getArticleTitle() ?></h3>
+                <p><?= $article->getArticleText() ?></p>
+                <p>Publié le <?= $article->getArticleDatePublish() ?> par <?= $article->getUser()->getUserFullName() ?></p>
+                <p>Categories:
+                    <?php
+                        if(is_null($article->getCategories())):
+                        ?>
+                    Aucune catégorie !
+                    <?php
+                        else:
+                            foreach ($article->getCategories() as $categorie):
+                     ?>
+
+                        <a href=".?route=categorie&slug=<?= $categorie->getCategorySlug()?>"><?= $categorie->getCategoryName() ?></a>
+                        <?php
+                             endforeach;
+                        endif;
+                    ?>
+                </p>
+                <p>Nombre de commentaires: <?= $article->getCommentCount() ?></p>
+                <hr>
+            </article>
+            <?php
+        endforeach;
+    endif;
+    ?>
+    <?php
     var_dump($articles,$categories);
     ?>
 </body>

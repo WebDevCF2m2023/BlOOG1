6a0ef9936df10b4824a4f37e2e584b0a43c00ec0
BailGui <guillaumebaillieux88@gmail.com>
2024-07-07 18:11:09 +0200

    workOnPublicHomePage

--- test/Guillaume/06-twig-public-view/view/publicView/public.homepage.php
+++ test/Guillaume/06-twig-public-view/view/publicView/public.homepage.php
@@ -1,15 +1,27 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Bloog 1 | Homepage</title>
-</head>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Blog Post - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
 <body>
-    <h1>Bloog 1 | Homepage</h1>
+     <!-- Page content-->
+     <div class="container mt-5">
+            <div class="row">
+                <div class="col-lg-8">
+                    <!-- Post content-->
+                    <article>
+                        <!-- Post header-->
+                        <header class="mb-4">
+                            <!-- Post title-->
+                            <h1 class="fw-bolder mb-1">Bloog 1 | Homepage</h1>
     <?php
     require 'public.menu.php';
     ?>
@@ -24,9 +36,9 @@
             ?>
             <article>
                 <h3><a href="?route=article&slug=<?= $article->getArticleSlug() ?>"><?= $article->getArticleTitle() ?></a></h3>
-                <p><?= $article->getArticleText() ?> <a href="?route=article&slug=<?= $article->getArticleSlug() ?>"> ... Lire la suite</a></p>
-                <p>Publié le <?= $article->getArticleDatePublish() ?> par <?= $article->getUser()->getUserFullName() ?></p>
-                <p>Categories:
+                <p class="fs-5 mb-4"><?= $article->getArticleText() ?> <a href="?route=article&slug=<?= $article->getArticleSlug() ?>"> ... Lire la suite</a></p>
+                <div class="text-muted fst-italic mb-2">Publié le <?= $article->getArticleDatePublish() ?> par <?= $article->getUser()->getUserFullName() ?></div>
+                <p class="fs-5 mb-4">Categories:
                     <?php
                         if(is_null($article->getCategories())):
                         ?>
@@ -42,13 +54,20 @@
                         endif;
                     ?>
                 </p>
-                <p>Nombre de commentaires: <?= $article->getCommentCount() ?></p>
+                <p class="fs-5 mb-4">Nombre de commentaires: <?= $article->getCommentCount() ?></p>
                 <hr>
             </article>
             <?php
         endforeach;
     endif;
     ?>
-
-</body>
+<!-- Footer-->
+<footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
 </html>
